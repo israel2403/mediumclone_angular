57e085156c617835d0d652102041446b3d3c2713
Israel Huerta <israelhf24@gmail.com>
2025-03-08 10:48:06 -0600

    feat: configure environment settings for development and update auth service URL

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
 import { map, Observable } from 'rxjs'
+import { environment } from '../../../environments/environment.development'
 import { CurrentUserInterface } from '../../shared/types/current-user.interface'
 import { AuthResponseInterface } from '../types/auth-response.interface'
 import { RegisterRequestInterface } from '../types/register-request.interface'
@@ -12,7 +13,7 @@ export class AuthService {
   constructor(private http: HttpClient) {}
 
   register(data: RegisterRequestInterface): Observable<CurrentUserInterface> {
-    const url = 'http://localhost:3000/api/users'
+    const url = environment.baseUrl + 'http://localhost:3000/api/users'
     return this.http
       .post<AuthResponseInterface>(url, data)
       .pipe(map((response: AuthResponseInterface) => response.user))
