57d699e57f12b750d4cde32c180b8d68cb6db106
Israel Huerta <israelhf24@gmail.com>
2025-03-08 12:00:05 -0600

    feat: add isLoading and validationErrors properties to AuthStateInterface for improved state management

--- src/app/auth/types/auth-state.interface.ts
+++ src/app/auth/types/auth-state.interface.ts
@@ -1,6 +1,9 @@
 import { CurrentUserInterface } from '../../shared/types/current-user.interface'
+import { BackendErrorsInterface } from './backend-errors.interface'
 
 export interface AuthStateInterface {
   isSubmitting: boolean
   currentUser: CurrentUserInterface | null | undefined
+  isLoading: boolean
+  validationErrors: BackendErrorsInterface | null
 }
