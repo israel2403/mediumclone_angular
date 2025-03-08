bef8b58b1100f3cca0cf1b995bd4f79e01f482bf
Israel Huerta <israelhf24@gmail.com>
2025-03-08 11:59:24 -0600

    feat: add currentUser property to AuthStateInterface for user state management

--- src/app/auth/types/auth-state.interface.ts
+++ src/app/auth/types/auth-state.interface.ts
@@ -1,3 +1,6 @@
+import { CurrentUserInterface } from '../../shared/types/current-user.interface'
+
 export interface AuthStateInterface {
   isSubmitting: boolean
+  currentUser: CurrentUserInterface | null | undefined
 }
