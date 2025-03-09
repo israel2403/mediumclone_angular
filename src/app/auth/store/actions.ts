141ab3c50624061fd8e461916ddb0468f01cf4e1
Israel Huerta <israelhf24@gmail.com>
2025-03-08 20:20:16 -0600

    feat: add login actions to auth store for handling login requests and responses

--- src/app/auth/store/actions.ts
+++ src/app/auth/store/actions.ts
@@ -1,6 +1,7 @@
 import { createActionGroup, props } from '@ngrx/store'
 import { CurrentUserInterface } from '../../shared/types/current-user.interface'
 import { BackendErrorsInterface } from '../types/backend-errors.interface'
+import { LoginRequestInterface } from '../types/login-request.interface'
 import { RegisterRequestInterface } from '../types/register-request.interface'
 
 export const authActions = createActionGroup({
@@ -9,5 +10,9 @@ export const authActions = createActionGroup({
     Register: props<{ request: RegisterRequestInterface }>(),
     'Register Success': props<{ currentUser: CurrentUserInterface }>(),
     'Register failure': props<{ errors: BackendErrorsInterface }>(),
+
+    Login: props<{ request: LoginRequestInterface }>(),
+    'Login Success': props<{ currentUser: CurrentUserInterface }>(),
+    'Login failure': props<{ errors: BackendErrorsInterface }>(),
   },
 })
