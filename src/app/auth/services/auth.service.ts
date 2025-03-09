5e5da6f677de9a839e0d8d0b913401fb2f90059a
Israel Huerta <israelhf24@gmail.com>
2025-03-08 20:16:50 -0600

    feat: add login functionality with request interface to authenticate users

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -4,6 +4,7 @@ import { map, Observable } from 'rxjs'
 import { environment } from '../../../environments/environment.development'
 import { CurrentUserInterface } from '../../shared/types/current-user.interface'
 import { AuthResponseInterface } from '../types/auth-response.interface'
+import { LoginRequestInterface } from '../types/login-request.interface'
 import { RegisterRequestInterface } from '../types/register-request.interface'
 
 @Injectable({
@@ -14,6 +15,13 @@ export class AuthService {
 
   register(data: RegisterRequestInterface): Observable<CurrentUserInterface> {
     const url = environment.baseUrl + '/users'
+    return this.http
+      .post<AuthResponseInterface>(url, data)
+      .pipe(map((response: AuthResponseInterface) => response.user))
+  }
+
+  login(data: LoginRequestInterface): Observable<CurrentUserInterface> {
+    const url = environment.baseUrl + '/users/login'
     return this.http
       .post<AuthResponseInterface>(url, data)
       .pipe(map((response: AuthResponseInterface) => response.user))
