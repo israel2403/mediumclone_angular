354694e4ffcf39fd0d4d493289f188f00a45d255
Israel Huerta <israelhf24@gmail.com>
2025-03-10 07:08:02 -0600

    feat: implement global feed component with basic structure

---
+++ src/app/global-feed/components/global-feed/global-feed.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mc-global-feed',
+  templateUrl: './global-feed.component.html',
+  standalone: true,
+  imports: [],
+})
+export class GlobalFeedComponent {}
