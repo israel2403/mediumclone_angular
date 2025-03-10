f391b02f52dea9f7f8f31994ea6cdf97c155b229
Israel Huerta <israelhf24@gmail.com>
2025-03-10 07:23:33 -0600

    feat: update getFeed method to return Observable<GetFeedResponseInterface>

--- src/app/shared/components/feed/services/feed.service.ts
+++ src/app/shared/components/feed/services/feed.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
+import { Observable } from 'rxjs'
 import { environment } from '../../../../../environments/environment.development'
 import { GetFeedResponseInterface } from '../types/get-feed-response.interface'
 
@@ -9,7 +10,7 @@ import { GetFeedResponseInterface } from '../types/get-feed-response.interface'
 export class FeedService {
   constructor(private http: HttpClient) {}
 
-  getFeed(url: string) {
+  getFeed(url: string): Observable<GetFeedResponseInterface> {
     const fullUrl = environment.baseUrl + url
     return this.http.get<GetFeedResponseInterface>(fullUrl)
   }
