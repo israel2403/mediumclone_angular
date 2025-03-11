89440c30f1c4acbe4a5d02973648949b9d61e988
Israel Huerta <israelhf24@gmail.com>
2025-03-11 11:54:20 -0600

    feat: add pagination component to feed for improved article navigation

--- src/app/shared/components/feed/feed.component.ts
+++ src/app/shared/components/feed/feed.component.ts
@@ -1,10 +1,13 @@
 import { CommonModule } from '@angular/common'
 import { Component, Input, OnInit } from '@angular/core'
-import { RouterLink } from '@angular/router'
+import { ActivatedRoute, Router, RouterLink } from '@angular/router'
 import { Store } from '@ngrx/store'
+import queryString from 'query-string'
 import { combineLatest } from 'rxjs'
+import { environment } from '../../../../environments/environment.development'
 import { ErrorMessageComponent } from '../error-message/error-message.component'
 import { LoadingComponent } from '../loading/loading.component'
+import { PaginationComponent } from '../pagination/pagination.component'
 import { feedActions } from './store/actions'
 import { selectError, selectFeedData, selectIsLoading } from './store/reducers'
 
@@ -12,11 +15,28 @@ import { selectError, selectFeedData, selectIsLoading } from './store/reducers'
   selector: 'mc-feed',
   templateUrl: './feed.component.html',
   standalone: true,
-  imports: [CommonModule, RouterLink, ErrorMessageComponent, LoadingComponent],
+  imports: [
+    CommonModule,
+    RouterLink,
+    ErrorMessageComponent,
+    LoadingComponent,
+    PaginationComponent,
+  ],
 })
 export class FeedComponent implements OnInit {
   @Input() apiUrl: string = ''
-  constructor(private store: Store) {}
+
+  limit = environment.limit
+
+  baseUrl = this.router.url.split('?')[0]
+
+  currentPage: number = 0
+
+  constructor(
+    private store: Store,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   data$ = combineLatest({
     isLoading: this.store.select(selectIsLoading),
@@ -25,6 +45,21 @@ export class FeedComponent implements OnInit {
   })
 
   ngOnInit(): void {
-    this.store.dispatch(feedActions.getFeed({ url: this.apiUrl }))
+    this.route.queryParams.subscribe((params) => {
+      this.currentPage = Number(params['page'] || '1')
+      this.fetchFeed()
+    })
+  }
+
+  fetchFeed() {
+    const offset = this.currentPage * this.limit - this.limit
+    const parsedUrl = queryString.parseUrl(this.apiUrl)
+    const stringifiedParams = queryString.stringify({
+      limit: this.limit,
+      offset,
+      ...parsedUrl.query,
+    })
+    const apiUrlWithParams = `${parsedUrl.url}?${stringifiedParams}`
+    this.store.dispatch(feedActions.getFeed({ url: apiUrlWithParams }))
   }
 }
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: false,
     baseUrl: 'http://localhost:3000/api',
+    limit: 2,
   }
   
