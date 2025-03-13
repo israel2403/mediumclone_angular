2f803012e09381e5a0a770dba974016410e8d6f6
Israel Huerta <israelhf24@gmail.com>
2025-03-13 07:33:58 -0600

    feat: add NgRx state management and effects to article component routing

--- src/app/article/article.routes.ts
+++ src/app/article/article.routes.ts
@@ -1,9 +1,16 @@
 import { Routes } from '@angular/router'
+import { provideEffects } from '@ngrx/effects'
+import { provideState } from '@ngrx/store'
 import { ArticleComponent } from './components/article/article.component'
-
+import * as articleEffects from './store/effects'
+import { articleFeatureKey, articleReducer } from './store/reducers'
 export const routes: Routes = [
   {
     path: '',
     component: ArticleComponent,
+    providers: [
+      provideEffects(articleEffects),
+      provideState(articleFeatureKey, articleReducer),
+    ],
   },
 ]
